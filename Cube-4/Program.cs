5a7ab98ab678134347d9f9ca47862603f48c80a0
Nathan <nathan.faucon@viacesi.fr>
2022-12-12 10:13:39 +0100

    premier push

--- Cube-4/Program.cs
+++ Cube-4/Program.cs
@@ -2,10 +2,10 @@ using Cube_4.Datas;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("Default"), new MySqlServerVersion(new Version(8, 0, 31))));
+options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+// Add services to the container.
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
